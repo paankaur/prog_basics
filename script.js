5d55dc91890b75a47fa39c4209b1bcf70065b487
Paan Kaur Riives <paan.kaur@gmail.com>
2024-09-27 15:38:59 +0300

    console.log practice and uncompleted prompt excercise

---
+++ script.js
@@ -0,0 +1,54 @@
+
+    //     // This my first java excercise // 
+    //     console.log('Hellou World');
+    // let firstName = 'Paan'; // string literal
+    // let lastName = 'Riives';
+    // console.log(firstName);
+    // console.log(lastName);
+
+    // let interestRate = 0.3;
+    // interestRate = 1;
+    // console.log(interestRate);
+
+    // let age = 31; //number literal
+    // let isApproved = false; //Boolean literal
+    // // let firstName = undefined; //idk mis ta VEEL on
+    // let selectedColor = null; //atm empty
+
+    // let person = {
+    //     firstName:'Paan',
+    //     age: 31
+    // };
+    // //Dot notation
+    // person.firstName = 'Paan';
+
+    // //bracket notation
+    // person[firstName] = 'Paan'
+    // let selection = 'Paan'
+    // person[selection] = 'Paan'
+
+    // console.log(person);
+
+    // let selectedColors = ['red','blue'];
+    // selectedColors[2] = 'green';
+    // console.log(selectedColors.length);
+
+     let ='a = ?';
+     prompt('a=?');
+     prompt('b=?');
+
+     document.write(sum)
+
+
+    function greet(name, lastName) {
+        console.log('hello' + name + lastName);
+    }
+    greet('John', 'Smith')
+
+    // calculating a value
+
+    function square(number) {
+        return number * number;
+    }
+    console.log(square(2));
+
