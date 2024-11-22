aeab9be0335e15ce3cb2a829dcaeb615268fcc9b
Paan Kaur Riives <paan.kaur@gmail.com>
2024-11-22 12:34:59 +0200

    väike ebaoluline muudatus

---
+++ objects.js
@@ -0,0 +1,94 @@
+// const person1 = {
+//   firstName: "Spongebob",
+//   lastName: "Squarepants",
+//   age: 30,
+//   isEmployed: true,
+//   sayHello: function () {
+//     console.log("Hi! Im Spongebob!");
+//   },
+//   eat: function () {
+//     console.log("I am eating a crabby patty");
+//   },
+// };
+
+// console.log(person1.firstName);
+// console.log(person1.lastName);
+// console.log(person1.age);
+// console.log(person1.isEmployed);
+
+// const person2 = {
+//   firstName: "Patrick",
+//   lastName: "Star",
+//   age: 45,
+//   isEmployed: false,
+//   sayHello: function () {
+//     console.log("Hi! Im Patrick ...");
+//   },
+//   eat: () => console.log("I am eating a pizza!!"),
+// };
+// console.log(person2.firstName);
+// console.log(person2.lastName);
+// console.log(person2.age);
+// console.log(person2.isEmployed);
+
+// person1.sayHello();
+// person2.sayHello();
+// person1.eat();
+// person2.eat();
+
+// function Car(make, model, year, color,) {
+//   (this.make = make),
+//     (this.model = model),
+//     (this.year = year),
+//     (this.color = color);
+// }
+// const car1 = new Car("Ford", "Mustang", 2026, "red");
+// console.log(car1.make);
+// console.log(car1.model);
+// console.log(car1.year);
+// console.log(car1.color);
+
+// class Product{
+//     constructor(name,price){
+//         this.name = name;
+//         this.price = price;
+//     }
+//     displayProduct(){
+//         console.log(`Product: ${this.name}`);
+//         console.log(`Price: $${this.price.toFixed(2)}`);
+//     }
+// }
+
+
+// class MathUtil{
+//     static PI = 3.14159;
+
+//     static getDiameter(radius){
+//         return radius * 2;
+//     }
+//     static getCircumference(radius){
+//         return radius * 2 ;
+//     }
+// }
+
+// console.log(MathUtil.PI);
+
+// console.log(MathUtil.getDiameter(10));
+// console.log(MathUtil.getCircumference(10));
+
+// pooleli
+
+class Rectangle{
+    constructor(width, height){
+        this.width = width;
+        this.height = height;
+    }
+    set width(newWidth){
+        if(newWidth > 0{
+            this._width = newWidth;
+        } else{})
+    }
+}
+
+const rectangle = new Rectangle(-1000000, "pizza");
+
